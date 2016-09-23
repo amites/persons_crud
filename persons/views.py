2d30840e5a4e51ad2fe2ada06bf09e9b1d261b77
Alvin Mites <alvin@mitesdesign.com>
2016-09-22 22:46:21 -0600

    added messages

--- persons/views.py
+++ persons/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -11,19 +12,24 @@ def update_persons(request, person_id=None):
         form = PersonForm(request.POST)
         if form.is_valid():
             form.save()
+            if request.POST.get('person_id'):
+                messages.success(request, 'Person updated.')
+            else:
+                messages.success(request, 'Person added.')
             return HttpResponseRedirect(reverse('persons:list'))
     elif person_id:
         try:
             obj = Person.objects.get(pk=person_id)
             form = PersonForm(instance=obj)
         except Person.DoesNotExist:
-            # add error message
+            messages.error(request, 'Person does not exist.')
             form = PersonForm()
     else:
         form = PersonForm()
 
     context = {
         'form': form,
+        'person_id': person_id,
     }
     return render(request, 'persons/create.html', context)
 
@@ -39,6 +45,9 @@ def delete_persons(request, person_id):
     try:
         obj = Person.objects.get(pk=person_id)
         obj.delete()
+        messages.success(
+            request, 'Person {first_name} {last_name} deleted.'.format(
+                **obj.__dict__))
     except Person.DoesNotExist:
-        pass
+        messages.error(request, 'Person does not exist.')
     return HttpResponseRedirect(reverse('persons:list'))
